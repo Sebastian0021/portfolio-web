ac07c430ea1b5da7cdb276d5f696a83b1aaa0af2
Sebastian0021 <sebastianalefuentespe@gmail.com>
2024-08-10 13:15:15 -0300

    Agregando componente About al porfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import About from './components/About'
 import Header from './components/Header'
 import Home from './components/Home'
+import Skills from './components/Skills'
 
 export default function App() {
   return (
     <>
       <Header></Header>
-      <main className="h-screen flex items-center">
+      <main className="h-screen max-w-5xl flex flex-col mx-auto">
         <Home></Home>
+        <About></About>
+        <Skills />
       </main>
     </>
   )
---
+++ src/components/Skills.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <h3>My technical level</h3>
+      <div></div>
+    </section>
+  )
+}
+
+export default Skills
